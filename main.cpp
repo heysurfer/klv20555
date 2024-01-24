f90e556d75958a76bd81a5ca42bdbb0a50575bfe
HeySurfer <HeySurfers@protonmail.com>
2024-01-24 05:58:43 +0300

    V4.47

--- main.cpp
+++ main.cpp
@@ -1,54 +1,15 @@
+#include <format>
 #include <cryptopp/md5.h>
 #include <cryptopp/hex.h>
-#include <cryptopp/sha.h>
 
-template <typename T>
-std::string toStr(const T& t)
+std::string get_md5_checksum(const std::string& str)
 {
-	std::ostringstream os;
-	os << t;
-	return os.str();
-}
-
-std::vector<std::string> split(const std::string& s, char delim)
-{
-	std::vector<std::string> result;
-	std::istringstream iss(s);
-
-	for (std::string token; std::getline(iss, token, delim);)
-	{
-		result.push_back(std::move(token));
-	}
-
-	return result;
-}
-
-std::string sha256(const std::string& text)
-{
-	CryptoPP::SHA256 hash;
-	std::string digest;
-
-	CryptoPP::StringSource(text, true,
-	                       new CryptoPP::HashFilter(hash,
-	                                                new CryptoPP::HexEncoder(
-		                                                new CryptoPP::StringSink(digest)
-	                                                )
-	                       )
-	);
-
-	return digest;
-}
-
-std::string GetMD5CheckSumAsString(const std::string& str)
-{
-	using byte = unsigned char;
-
 	CryptoPP::Weak1::MD5 md5;
-	byte digest[CryptoPP::Weak1::MD5::DIGESTSIZE];
-	md5.CalculateDigest(digest, reinterpret_cast<const byte*>(str.c_str()), str.length());
+	unsigned char digest[CryptoPP::Weak1::MD5::DIGESTSIZE];
+	md5.CalculateDigest(digest, reinterpret_cast<const unsigned char*>(str.c_str()), str.length());
 
-	CryptoPP::HexEncoder encoder;
 	std::string encoded;
+	CryptoPP::HexEncoder encoder;
 	encoder.Attach(new CryptoPP::StringSink(encoded));
 	encoder.Put(digest, sizeof(digest));
 	encoder.MessageEnd();
@@ -56,70 +17,64 @@ std::string GetMD5CheckSumAsString(const std::string& str)
 	return encoded;
 }
 
-std::string xor_decrypt(uint8_t* encryptedStr, uint8_t pos, uint32_t key)
-{
+std::string xor_decrypt(const std::uint8_t* encrypted_str, std::size_t pos, std::uint32_t key) {
 	std::string str;
-	std::size_t size = 32;
-	str.reserve(size);
-
-	for (std::size_t i = 0; i < size; ++i)
-	{
-		str.push_back(encryptedStr[i + pos] ^ key);
-		key = ((key << 1) | (key >> 31));
+	str.reserve(32);
+	for (std::size_t i = 0; i < 32; ++i) {
+		str.push_back(encrypted_str[i + pos] ^ key);
+		key = (key << 1) | (key >> 31);
 	}
 	return str;
 }
 
-std::string generateKlv(float gameVersion, uint32_t protocol, const std::string& rid, int32_t hash,
-                        const std::vector<std::string>& salts)
-{
-	std::string klv = GetMD5CheckSumAsString(GetMD5CheckSumAsString(std::to_string(gameVersion)));
-	klv.append((salts[0]));
-	klv.append(GetMD5CheckSumAsString(GetMD5CheckSumAsString(GetMD5CheckSumAsString(std::to_string(protocol)))));
-	klv.append((salts[1]));
-	klv.append((salts[2]));
-	klv.append(GetMD5CheckSumAsString(GetMD5CheckSumAsString(rid)));
-	klv.append((salts[3]));
-	klv.append(GetMD5CheckSumAsString(GetMD5CheckSumAsString(std::to_string(hash))));
-	klv.append((salts[4]));
-
-	return GetMD5CheckSumAsString(klv);
+std::string generate_klv(float game_version, std::uint32_t protocol, const std::string& rid, int32_t hash,
+	const std::vector<std::string>& salts) {
+	return get_md5_checksum(std::format(
+		"{}{}{}{}{}{}{}{}{}",
+		get_md5_checksum(get_md5_checksum(std::to_string(game_version))),
+		salts[0],
+		get_md5_checksum(get_md5_checksum(get_md5_checksum(std::to_string(protocol)))),
+		salts[1],
+		salts[2],
+		get_md5_checksum(get_md5_checksum(rid)),
+		salts[3],
+		get_md5_checksum(get_md5_checksum(std::to_string(hash))),
+		salts[4]
+	));
 }
 
 int main()
 {
-	std::vector keys{-91863646, -969031837, 1103616536, 943360009, -1982213226};
-	std::vector<std::string> salts;
-	salts.reserve(5);
-	uint8_t encryptedStr[]
+	std::vector keys{1898659612, -27235823, 328180306, 57007415, -1767811648};
+	std::vector<std::string> salts(5);
+	constexpr uint8_t encryptedStr[]
 	{
-		0x9A, 0x76, 0xB9, 0x76, 0x4E, 0x3C, 0x8E, 0x4A, 0xCB, 0x93,
-		0x8C, 0xB6, 0xCB, 0x36, 0xC2, 0x72, 0xB3, 0x6E, 0x7F, 0x57,
-		0x55, 0xAC, 0xA1, 0x43, 0x72, 0xEA, 0x72, 0x1C, 0x6F, 0x86,
-		0x5A, 0xE0, 0x54, 0xF7, 0xB6, 0x2C, 0x05, 0x4E, 0x95, 0x87,
-		0xA2, 0xBC, 0x2C, 0x57, 0x00, 0xF3, 0xBF, 0x29, 0x09, 0x1A,
-		0xC1, 0x8F, 0xEF, 0x83, 0x5C, 0xBA, 0xDC, 0x1D, 0xCD, 0xCC,
-		0xCF, 0xD5, 0xB9, 0xD0, 0x2E, 0x02, 0x52, 0xA6, 0xE1, 0x39,
-		0x75, 0x18, 0x27, 0xE5, 0x61, 0x3C, 0x2E, 0x59, 0x43, 0x81,
-		0xF4, 0xEA, 0x2F, 0x5A, 0x4A, 0x9E, 0xC6, 0xDC, 0xEF, 0x8D,
-		0x5B, 0xB5, 0x96, 0x20, 0xB4, 0x3E, 0x6B, 0x70, 0x1C, 0x7A,
-		0xA6, 0x42, 0x7B, 0xFD, 0x0D, 0x47, 0x85, 0xF7, 0xE0, 0x3F,
-		0x36, 0x78, 0x08, 0x41, 0xDE, 0xED, 0x9C, 0x68, 0x9B, 0x27,
-		0xE9, 0x24, 0x18, 0x77, 0xE3, 0x64, 0x66, 0x36, 0xF3, 0x4C,
-		0x6D, 0x82, 0x0D, 0xE5, 0xC6, 0x72, 0xB9, 0x23, 0x1E, 0x7C,
-		0xA5, 0x09, 0x47, 0xD4, 0xEF, 0x83, 0x51, 0xFD, 0xFB, 0x5F,
-		0x4D, 0xDD, 0xE5, 0x92, 0x28, 0xAE, 0x5D, 0x4B, 0x84, 0xA9
+		0x24, 0x0B, 0x43, 0x82, 0xA6, 0xED, 0x2C, 0x0F, 0x40, 0x84,
+		0xA2, 0xEB, 0x71, 0x43, 0x29, 0xA4, 0x1E, 0x34, 0xCA, 0x3C,
+		0x82, 0x03, 0xFF, 0xFE, 0x08, 0x1F, 0x98, 0xC9, 0xC4, 0xD1,
+		0xF5, 0xBF, 0x26, 0x13, 0x7E, 0xBD, 0x26, 0x09, 0x1B, 0x9B,
+		0x9A, 0xCC, 0xCD, 0x95, 0x85, 0xF8, 0xA8, 0x07, 0x54, 0xA1,
+		0xB6, 0x61, 0x32, 0x39, 0x29, 0x50, 0x09, 0xB7, 0x98, 0x67,
+		0x98, 0x7B, 0xE5, 0x69, 0x64, 0x96, 0x7B, 0xF4, 0x44, 0x73,
+		0xE1, 0x31, 0x75, 0x41, 0x28, 0xAE, 0x0A, 0x10, 0xD1, 0xA5,
+		0xBC, 0x7A, 0x0E, 0x19, 0xCD, 0x91, 0xD8, 0xE0, 0x97, 0x7D,
+		0xBA, 0x77, 0x12, 0x29, 0xA6, 0x1B, 0x55, 0x0C, 0xE4, 0x8B,
+		0x45, 0x85, 0xF5, 0xE0, 0x36, 0x31, 0x68, 0x2D, 0x55, 0x54,
+		0xE1, 0xD7, 0x57, 0xFF, 0xA3, 0x17, 0x69, 0x82, 0x4E, 0x8D,
+		0xBC, 0x8E, 0x4C, 0xDE, 0xB0, 0xC3, 0x29, 0xA9, 0xA5, 0xE0,
+		0x35, 0x32, 0x6C, 0x26, 0x41, 0x7D, 0xA6, 0x1D, 0x63, 0x87,
+		0x52, 0xE6, 0x99, 0x68, 0x97, 0x72, 0xB3, 0x39, 0x73, 0x4F,
+		0x6C, 0x9E, 0x63, 0x9E, 0x31, 0x9C, 0x38, 0x81, 0x11, 0x82
 	};
 	static_assert(sizeof(encryptedStr) >= 160, "Encrypted string size must be at least 160");
 
 	for (auto key : keys)
 	{
-		int index = std::distance(keys.begin(), std::find(keys.begin(), keys.end(), key));
+		int index = std::distance(keys.begin(), std::ranges::find(keys, key));
 		std::string decSalt = xor_decrypt(encryptedStr, index * 32, key);
 		std::printf("%i.Salt : %s\n", index, decSalt.c_str());
 		salts.push_back(decSalt);
 	}
-
 	std::printf("Generated Klv %s\n",
-	            generateKlv(4.38f, 200, "0182E268D08AB45702A841A172170E12", 1431658473, salts).c_str());
+		generate_klv(4.47f, 0xC9, "0182E268D08AB45702A841A172170E12", 1431658473, salts).c_str());
 }
